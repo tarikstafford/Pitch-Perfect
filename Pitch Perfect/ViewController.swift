4c862c0486aba454c09f66a87d0ecc819509cc51
Tarik Stafford <tstafford88@gmail.com>
2017-01-17 13:07:00 +0530

    Added Button Images & Logic

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RecordingLabel: UILabel!
+    @IBOutlet weak var RecordingButton: UIButton!
+    @IBOutlet weak var StopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        StopRecordingButton.isEnabled = false
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        RecordingButton.isEnabled = true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func RecordButton(_ sender: Any) {
+        RecordingLabel.text = "Recording"
+        StopRecordingButton.isEnabled = true
+        RecordingButton.isEnabled = false
+    }
+    @IBAction func StopRecording(_ sender: Any) {
+        RecordingButton.isEnabled = true
+        StopRecordingButton.isEnabled = false
+        RecordingLabel.text = "Tap to Record"
+    }
 
 }
 
